92e7a94abb803aef7366aaf98986105ff6ac2dc6
Luis Castro Martín <luiscastro193@gmail.com>
2026-05-13 11:29:47 +0200

    Log errors

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 "use strict";
 import hash from './hasher.js';
 
-const input = document.querySelector("input");
-const result = document.querySelector("p");
+const input = document.querySelector('input');
+const result = document.querySelector('p');
 
 input.onchange = async () => {
 	input.disabled = true;
-	result.textContent = 'Loading...';
-	result.textContent = await hash(input.files[0].stream());
+	result.textContent = "Loading...";
+	
+	result.textContent = await hash(input.files[0].stream()).catch(e => {
+		console.error(e);
+		return "Error";
+	});
+	
 	input.disabled = false;
 	input.focus();
 }
