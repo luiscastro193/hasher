b7061e0e9d51923ca31e6530f767061c7285d3dd
luiscastro193 <luiscastro193@gmail.com>
2026-05-16 01:39:54 +0200

    Use full WritableStream API

--- hasher.js
+++ hasher.js
@@ -2,17 +2,21 @@ import module from './hash.js';
 const hasherPromise = module();
 
 export default async function hash(stream) {
-	const hasher = await hasherPromise;
-	const state = hasher._create();
+	let hasher, state, result;
 	
-	await stream.pipeTo(new WritableStream({write: chunk => {
-		const buffer = hasher._allocate(state, chunk.length);
-		hasher.HEAPU8.set(chunk, buffer);
-		hasher._update(state, buffer, chunk.length);
-	}})).catch(error => {
-		hasher._digest(state);
-		throw error;
-	});
+	await stream.pipeTo(new WritableStream({
+		start: async () => {
+			hasher = await hasherPromise;
+			state = hasher._create();
+		},
+		write: chunk => {
+			const buffer = hasher._allocate(state, chunk.length);
+			hasher.HEAPU8.set(chunk, buffer);
+			hasher._update(state, buffer, chunk.length);
+		},
+		close: () => {result = hasher._digest(state)},
+		abort: () => {hasher._digest(state)}
+	}, {highWaterMark: 2}));
 	
-	return hasher._digest(state);
+	return result;
 }
