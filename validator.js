c27cdd05c2b87bf147b1a345fe4237eb0876b88d
luiscastro193 <luiscastro193@gmail.com>
2026-06-11 14:03:49 +0200

    Add string validator

---
+++ validator.js
@@ -0,0 +1,22 @@
+"use strict";
+import hash from './hasher.js';
+
+const input = document.querySelector('textarea');
+const result = document.querySelector('p');
+
+input.oninput = async () => {
+	const text = input.value.trim();
+	
+	if (text) {
+		result.textContent = await hash(new Blob([text]).stream()).then(h => 
+			BigInt.asUintN(64, h)
+		).catch(e => {
+			console.error(e);
+			return "Error";
+		});
+	}
+	else
+		result.textContent = '';
+}
+
+if (input.value) input.oninput();
